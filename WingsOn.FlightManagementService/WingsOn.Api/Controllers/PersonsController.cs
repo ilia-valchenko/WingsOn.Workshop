afdd9084d17e5ab6cc6b3adb268bf3402555b0fb
Ilya Valchanka <IValchanka@cpaglobal.com>
2019-12-06 23:46:19 +0300

    Implemented PersonsController.

---
+++ WingsOn.FlightManagementService/WingsOn.Api/Controllers/PersonsController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WingsOn.Api.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class PersonsController : ControllerBase
+    {
+        private readonly IPersonService _personService;
+
+        public PersonsController(IPersonService personService)
+        {
+            _personService = personService;
+        }
+
+        // GET: api/v1/persons
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var persons = _personService.GetAllPersons();
+            return Ok(persons);
+        }
+
+        // GET: api/v1/persons/5
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var person = _personService.GetPerson(id);
+
+            if (person == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(person);
+        }
+
+        // POST: api/v1/persons
+        [HttpPost]
+        public IActionResult Post([FromBody] Person person)
+        {
+            var result = _personService.CreatePerson(person);
+            return Ok(result);
+        }
+
+        // PUT: api/v1/persons/5
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] Person person)
+        {
+            _personService.UpdatePerson(person);
+            return Ok();
+        }
+
+        // DELETE: api/v1/persons/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _personService.RemovePerson(id);
+            return Ok();
+        }
+    }
+}
