90c43a3429c75898e9db2b60cc63fcc5ab4d3e56
Ilya Valchanka <IValchanka@cpaglobal.com>
2019-12-07 11:36:40 +0300

    Implemented PersonValidationService.

---
+++ WingsOn.FlightManagementService/WingsOn.Services/Interfaces/IPersonValidationService.cs
@@ -0,0 +1,9 @@
+﻿using WingsOn.Services.Models;
+
+namespace WingsOn.Services.Interfaces
+{
+    public interface IPersonValidationService
+    {
+        void ValidatePerson(PersonModel person);
+    }
+}
---
+++ WingsOn.FlightManagementService/WingsOn.Services/Services/PersonValidationService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using WingsOn.Core.Exceptions;
+using WingsOn.Services.Interfaces;
+using WingsOn.Services.Models;
+
+namespace WingsOn.Services.Services
+{
+    public sealed class PersonValidationService : IPersonValidationService
+    {
+        public void ValidatePerson(PersonModel person)
+        {
+            if (person == null)
+            {
+                throw new ArgumentNullException(nameof(person));
+            }
+
+            if (person.Id <= 0)
+            {
+                throw new InvalidResourceException("The person's id is less then zero or equal to zero.");
+            }
+        }
+    }
+}
