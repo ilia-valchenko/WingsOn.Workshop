1dc5576b3d037217cd9f6bfb60efb2cfda0924d2
Ilya Valchanka <IValchanka@cpaglobal.com>
2019-12-07 16:34:43 +0300

    Implemented PersonValidationService.

--- WingsOn.FlightManagementService/WingsOn.Services/Interfaces/IPersonService.cs
+++ WingsOn.FlightManagementService/WingsOn.Services/Interfaces/IPersonService.cs
@@ -3,16 +3,47 @@ using WingsOn.Services.Models;
 
 namespace WingsOn.Services.Interfaces
 {
+    /// <summary>
+    /// The person service interface.
+    /// </summary>
     public interface IPersonService
     {
+        /// <summary>
+        /// Gets all persons.
+        /// </summary>
+        /// <returns>
+        /// Returns a collection of <see cref="PersonModel"/> class.
+        /// </returns>
         IEnumerable<PersonModel> GetAllPersons();
 
+        /// <summary>
+        /// Creates a new person.
+        /// </summary>
+        /// <param name="person">The person model.</param>
+        /// <returns>
+        /// Returns an instance of the <see cref="PersonModel"/> class.
+        /// </returns>
         PersonModel CreatePerson(PersonModel person);
 
+        /// <summary>
+        /// Gets person by id.
+        /// </summary>
+        /// <param name="id">The person's identifier.</param>
+        /// <returns>
+        /// Returns an instance of the <see cref="PersonModel"/> class.
+        /// </returns>
         PersonModel GetPerson(int id);
 
+        /// <summary>
+        /// Updates a person.
+        /// </summary>
+        /// <param name="person">The updated person model.</param>
         void UpdatePerson(PersonModel person);
 
+        /// <summary>
+        /// Removes a person by provided id.
+        /// </summary>
+        /// <param name="id">The person's identifier.</param>
         void RemovePerson(int id);
     }
 }
--- WingsOn.FlightManagementService/WingsOn.Services/Services/PersonService.cs
+++ WingsOn.FlightManagementService/WingsOn.Services/Services/PersonService.cs
@@ -1,35 +1,166 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using WingsOn.Core.Exceptions;
+using WingsOn.Dal.Interfaces;
+using WingsOn.Domain;
+using WingsOn.Services.Infrastructure.Interfaces;
 using WingsOn.Services.Interfaces;
 using WingsOn.Services.Models;
 
 namespace WingsOn.Services.Services
 {
+    /// <summary>
+    /// The person service.
+    /// </summary>
     public sealed class PersonService : IPersonService
     {
+        private static readonly object _lockObject = new object();
+
+        // TODO: Use cacheable provider instead of repository.
+        private readonly IRepository<Person> _personRepository;
+        private readonly IResourceIdGenerator _resourceIdGenerator;
+        private readonly IPersonValidationService _personValidationService;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PersonService"/> class.
+        /// </summary>
+        /// <param name="personRepository"></param>
+        /// <param name="resourceIdGenerator"></param>
+        /// <param name="personValidationService"></param>
+        public PersonService(
+            IRepository<Person> personRepository,
+            IResourceIdGenerator resourceIdGenerator,
+            IPersonValidationService personValidationService)
+        {
+            _personRepository = personRepository;
+            _resourceIdGenerator = resourceIdGenerator;
+            _personValidationService = personValidationService;
+        }
+
+        /// <inheritdoc />
         public IEnumerable<PersonModel> GetAllPersons()
         {
-            throw new NotImplementedException();
+            var persons = _personRepository.GetAll();
+
+            // TODO: Use AutoMapper.
+            return persons.Select(MapToPersonModel);
         }
 
+        /// <inheritdoc />
         public PersonModel CreatePerson(PersonModel person)
         {
-            throw new NotImplementedException();
+            if (person == null)
+            {
+                throw new ArgumentNullException(nameof(person));
+            }
+
+            _personValidationService.ValidatePerson(person);
+
+            if (DoesPersonExist(person.Id))
+            {
+                throw new ResourceAlreadyExistException("The provided person already exists.");
+            }
+
+            if (person.Id == 0)
+            {
+                person.Id = GenerateNextPersonId();
+            }
+
+            // TODO: Use AutoMapper.
+            _personRepository.Save(MapToPerson(person));
+
+            return person;
         }
 
+        /// <inheritdoc />
         public PersonModel GetPerson(int id)
         {
-            throw new NotImplementedException();
+            var person = _personRepository.Get(id);
+
+            // TODO: Use AutoMapper.
+            return MapToPersonModel(person);
         }
 
+        /// <inheritdoc />
         public void UpdatePerson(PersonModel person)
         {
-            throw new NotImplementedException();
+            if (person == null)
+            {
+                throw new ArgumentNullException(nameof(person));
+            }
+
+            _personValidationService.ValidatePerson(person);
+            _personRepository.Save(MapToPerson(person));
         }
 
+        /// <inheritdoc />
         public void RemovePerson(int id)
         {
-            throw new NotImplementedException();
+            if (id <= 0)
+            {
+                throw new ArgumentException("The provided identifier is less then zero or equals to zero.");
+            }
+
+            _personRepository.Remove(id);
+        }
+
+        /// <summary>
+        /// Checks if a person exists.
+        /// </summary>
+        /// <param name="id">The person's identifier.</param>
+        /// <returns>Returns true if a person exists.</returns>
+        private bool DoesPersonExist(int id)
+        {
+            lock (_lockObject)
+            {
+                var persons = _personRepository.GetAll();
+
+                return persons == null || !persons.Any()
+                    ? false
+                    : persons.Any(p => p.Id == id);
+            }
+        }
+
+        /// <summary>
+        /// Generates the next person's id.
+        /// </summary>
+        /// <returns>
+        /// Returns the next person's id.
+        /// </returns>
+        private int GenerateNextPersonId()
+        {
+            lock (_lockObject)
+            {
+                var persons = _personRepository.GetAll();
+                return _resourceIdGenerator.GenerateResourceId(persons);
+            }
+        }
+
+        private Person MapToPerson(PersonModel personModel)
+        {
+            return new Person
+            {
+                Id = personModel.Id,
+                Name = personModel.Name,
+                Gender = personModel.Gender,
+                Email = personModel.Email,
+                Address = personModel.Address,
+                DateBirth = personModel.DateBirth
+            };
+        }
+
+        private PersonModel MapToPersonModel(Person person)
+        {
+            return new PersonModel
+            {
+                Id = person.Id,
+                Name = person.Name,
+                Gender = person.Gender,
+                Email = person.Email,
+                Address = person.Address,
+                DateBirth = person.DateBirth
+            };
         }
     }
 }
