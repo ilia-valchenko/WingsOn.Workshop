e975e23a5fa893504743189628c84c3c89adc595
Ilya Valchanka <IValchanka@cpaglobal.com>
2019-12-07 23:31:30 +0300

    Implemented FlightService.

---
+++ WingsOn.FlightManagementService/WingsOn.Services/Interfaces/IFlightService.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using WingsOn.Core.Enums;
+using WingsOn.Services.Models;
+
+namespace WingsOn.Services.Interfaces
+{
+    /// <summary>
+    /// The flight service interface.
+    /// </summary>
+    public interface IFlightService
+    {
+        /// <summary>
+        /// Returns all flights.
+        /// </summary>
+        /// <returns>
+        /// Returns a collection of instances of the <see cref="FlightModel"/> class.
+        /// </returns>
+        IEnumerable<FlightModel> GetAllFlights();
+
+        /// <summary>
+        /// Gets a flight by id.
+        /// </summary>
+        /// <param name="id">The flight's identifier.</param>
+        /// <returns>
+        /// Returns an instance of the <see cref="FlightModel"/> class.
+        /// </returns>
+        FlightModel GetFlight(int id);
+
+        /// <summary>
+        /// Gets a flight by a flight number.
+        /// </summary>
+        /// <param name="flightNumber">The number of a flight.</param>
+        /// <returns>
+        /// Returns an instance of the <see cref="FlightModel"/> class.
+        /// </returns>
+        FlightModel GetFlight(string flightNumber);
+
+        /// <summary>
+        /// Gets a list of passengers for a particular flight.
+        /// </summary>
+        /// <param name="flightNumber">The number of a flight.</param>
+        /// <param name="gender">The gender.</param>
+        /// <returns>
+        /// Returns a collection of instances of the <see cref="PersonModel"/> class.
+        /// </returns>
+        IEnumerable<PersonModel> GetPassengers(string flightNumber, GenderType? gender);
+    }
+}
---
+++ WingsOn.FlightManagementService/WingsOn.Services/Services/FlightService.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AutoMapper;
+using WingsOn.Core.Enums;
+using WingsOn.Core.Exceptions;
+using WingsOn.Dal.Interfaces;
+using WingsOn.Domain;
+using WingsOn.Services.Interfaces;
+using WingsOn.Services.Models;
+
+namespace WingsOn.Services.Services
+{
+    /// <summary>
+    /// The flight service.
+    /// </summary>
+    public sealed class FlightService : IFlightService
+    {
+        private readonly IRepository<Flight> _flightRepository;
+        private readonly IRepository<Booking> _bookingRepository;
+        private readonly IMapper _mapper;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FlightService"/> class.
+        /// </summary>
+        /// <param name="flightRepository">The flight repository.</param>
+        /// <param name="bookingRepository">The booking repository.</param>
+        /// <param name="mapper">The mapper.</param>
+        public FlightService(
+            IRepository<Flight> flightRepository,
+            IRepository<Booking> bookingRepository,
+            IMapper mapper)
+        {
+            _flightRepository = flightRepository;
+            _bookingRepository = bookingRepository;
+            _mapper = mapper;
+        }
+
+        /// <inheritdoc />
+        public IEnumerable<FlightModel> GetAllFlights()
+        {
+            var flights = _flightRepository.GetAll();
+            return _mapper.Map<IEnumerable<FlightModel>>(flights);
+        }
+
+        /// <inheritdoc />
+        public FlightModel GetFlight(int id)
+        {
+            if (id <= 0)
+            {
+                throw new ArgumentException("The flight's identifier is less or equal to zero.'");
+            }
+
+            var flight = _flightRepository.Get(id);
+            return _mapper.Map<FlightModel>(flight);
+        }
+
+        /// <inheritdoc />
+        public FlightModel GetFlight(string flightNumber)
+        {
+            if (string.IsNullOrWhiteSpace(flightNumber))
+            {
+                throw new ArgumentException("The number of the flight is not valid.");
+            }
+
+            var flight = _flightRepository.Find(f => string.Equals(f.Number, flightNumber, StringComparison.OrdinalIgnoreCase));
+            return _mapper.Map<FlightModel>(flight);
+        }
+
+        /// <inheritdoc />
+        public IEnumerable<PersonModel> GetPassengers(string flightNumber, GenderType? gender)
+        {
+            if (string.IsNullOrWhiteSpace(flightNumber))
+            {
+                throw new ArgumentException("The number of the flight is not valid.");
+            }
+
+            var flight = _flightRepository.Find(f => string.Equals(f.Number, flightNumber, StringComparison.OrdinalIgnoreCase));
+
+            if (flight == null)
+            {
+                throw new ResourceNotFoundException("The flight with provided flight number does not exist.");
+            }
+
+            var booking = _bookingRepository.Find(b =>
+                string.Equals(b.Flight?.Number, flightNumber, StringComparison.OrdinalIgnoreCase));
+
+            if (booking?.Passengers == null)
+            {
+                return Enumerable.Empty<PersonModel>();
+            }
+
+            var passengers = gender.HasValue
+                ? booking.Passengers.Where(p => p.Gender == gender.Value)
+                : booking.Passengers;
+
+            return _mapper.Map<IEnumerable<PersonModel>>(passengers);
+        }
+    }
+}
