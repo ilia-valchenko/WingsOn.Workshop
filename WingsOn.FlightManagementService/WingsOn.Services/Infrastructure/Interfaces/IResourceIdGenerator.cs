89c2b52e8ee642914adf1d6e6ac30bd15cdde34d
Ilya Valchanka <IValchanka@cpaglobal.com>
2019-12-07 11:25:56 +0300

    Implemented resource identifier generator.

---
+++ WingsOn.FlightManagementService/WingsOn.Services/Infrastructure/Interfaces/IResourceIdGenerator.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using WingsOn.Services.Models;
+
+namespace WingsOn.Services.Infrastructure.Interfaces
+{
+    public interface IResourceIdGenerator
+    {
+        int GenerateResourceId(IEnumerable<BaseModel> resources);
+    }
+}
---
+++ WingsOn.FlightManagementService/WingsOn.Services/Infrastructure/ResourceIdGenerator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using WingsOn.Services.Infrastructure.Interfaces;
+using WingsOn.Services.Models;
+
+namespace WingsOn.Services.Infrastructure
+{
+    public sealed class ResourceIdGenerator : IResourceIdGenerator
+    {
+        // TODO: Put this value in a configuration file.
+        // TODO: Use a configuration provider.
+        // TODO (optional): Create custom config section.
+        private const int IdIncrementingValue = 1;
+
+        public int GenerateResourceId(IEnumerable<BaseModel> resources)
+        {
+            if (resources == null)
+            {
+                throw new ArgumentNullException(nameof(resources));
+            }
+
+            return !resources.Any()
+                ? IdIncrementingValue
+                : resources.Select(p => p.Id).Max() + IdIncrementingValue;
+        }
+    }
+}
