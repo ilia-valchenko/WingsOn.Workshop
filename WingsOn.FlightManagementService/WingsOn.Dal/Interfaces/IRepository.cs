652bac78736ebdde8d685fe54caecca0150baa29
Ilya Valchanka <IValchanka@cpaglobal.com>
2019-12-07 16:33:03 +0300

    Extended base repository.

--- WingsOn.FlightManagementService/WingsOn.Dal/Interfaces/IRepository.cs
+++ WingsOn.FlightManagementService/WingsOn.Dal/Interfaces/IRepository.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using WingsOn.Domain;
 
 namespace WingsOn.Dal.Interfaces
@@ -27,9 +28,33 @@ namespace WingsOn.Dal.Interfaces
         T Get(int id);
 
         /// <summary>
+        /// Finds resource by predicate.
+        /// </summary>
+        /// <param name="predicate">The predicate.</param>
+        /// <returns>
+        /// Returns a single resource that satisfies predicate's condition.
+        /// </returns>
+        T Find(Predicate<T> predicate);
+
+        /// <summary>
+        /// Finds all resources that satisfy predicate's condition.
+        /// </summary>
+        /// <param name="predicate">The predicate.</param>
+        /// <returns>
+        /// Returns a collection of resources that satisfies predicates's condition.
+        /// </returns>
+        IEnumerable<T> FindAll(Predicate<T> predicate);
+
+        /// <summary>
         /// Saves a resource.
         /// </summary>
         /// <param name="element">The resource.</param>
         void Save(T element);
+
+        /// <summary>
+        /// Removes a resources by using provided id.
+        /// </summary>
+        /// <param name="id">The identifier of a resource that should be removed.</param>
+        void Remove(int id);
     }
 }
--- WingsOn.FlightManagementService/WingsOn.Dal/Repositories/RepositoryBase.cs
+++ WingsOn.FlightManagementService/WingsOn.Dal/Repositories/RepositoryBase.cs
@@ -1,29 +1,66 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using WingsOn.Core.Exceptions;
 using WingsOn.Dal.Interfaces;
 using WingsOn.Domain;
 
 namespace WingsOn.Dal.Repositories
 {
+    /// <summary>
+    /// The base repository.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
     public class RepositoryBase<T> : IRepository<T> where T : DomainObject
     {
+        /// <summary>
+        /// The collection of resources.
+        /// </summary>
+        protected List<T> Repository;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RepositoryBase{T}"/> class.
+        /// </summary>
         protected RepositoryBase()
         {
             Repository = new List<T>();
         }
 
-        protected List<T> Repository;
-
+        /// <inheritdoc />
         public IEnumerable<T> GetAll()
         {
             return Repository;
         }
 
+        /// <inheritdoc />
         public T Get(int id)
         {
             return GetAll().SingleOrDefault(a => a.Id == id);
         }
 
+        /// <inheritdoc />
+        public T Find(Predicate<T> predicate)
+        {
+            if (predicate == null)
+            {
+                throw new ArgumentNullException(nameof(predicate));
+            }
+
+            return Repository.Find(predicate);
+        }
+
+        /// <inheritdoc />
+        public IEnumerable<T> FindAll(Predicate<T> predicate)
+        {
+            if (predicate == null)
+            {
+                throw new ArgumentNullException(nameof(predicate));
+            }
+
+            return Repository.FindAll(predicate);
+        }
+
+        /// <inheritdoc />
         public void Save(T element)
         {
             if (element == null)
@@ -39,6 +76,19 @@ namespace WingsOn.Dal.Repositories
             }
 
             Repository.Add(element);
+        }
+
+        /// <inheritdoc />
+        public void Remove(int id)
+        {
+            T item = Get(id);
+
+            if (item == null)
+            {
+                throw new ResourceNotFoundException("Resource with provided id does not exist.");
+            }
+
+            Repository.Remove(item);
         }
     }
 }
